9c314189ae3264aaa197000fbcd6e177c947eced
kolepcruz <kikeponce.cruz@gmail.com>
2021-04-09 21:40:44 -0300

    🎨 Linting code

--- Blackjack.py
+++ Blackjack.py
@@ -1,10 +1,36 @@
 import random
-start=1
-valor = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-c = 0
-cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
-deck=[]
-if start == 1:
+
+class Player:
+    def __init__(self, name,hand,money):   #s
+        self.name = name
+        self.hand = hand
+        self.money = money
+    def see_hand(self):
+        print(f"==={self.name}'s Hand===")
+        for e in self.hand:
+            print(e)
+    def bet_money(self):
+        bet = float(input('Quanto quieres apostar?:'))
+        if bet > 0:
+            self.money -= bet
+    def buy_card(self):
+        buy = deck.pop(0)
+        self.hand.append(buy)
+
+class Dealer:
+    def embaralhar_Cartas(self,deck):
+        random.shuffle(deck)
+    def deal_Cards(self,players,deck):
+        for n in players:
+            carta1 = deck.pop(0)
+            carta2 = deck.pop(0)
+            n.hand.append(carta1)
+            n.hand.append(carta2)
+
+def start_deck():
+    
+    cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+    deck = []
     naipe = 0
     while naipe < 4:
         c = 0
@@ -16,44 +42,27 @@ if start == 1:
             deck.append(decksuits[naipe])
             c += 1
         naipe += 1
+    return deck
+
+deck=start_deck()
+
 print(len(deck))
-class Player:
-    def __init__(self, name,hand,money):   #s
-        self.name = name
-        self.hand = hand
-        self.money = money
-    def see_Hand(self):
-        print(f"==={self.name}'s Hand===")
-        for e in self.hand:
-            print(e)
-    def bet_Money(self):
-        bet = float(input('Quanto quieres apostar?:'))
-        if bet > 0:
-            self.money -= bet
-    def buy_card(self):
-        buy = deck.pop(0)
-        self.hand.append(buy)
 
-player1 = Player('Pierce', [], 500)
-player2 = Player('Margaret',[], 500)
-player3 = Player('Cubano', [], 500)
-playerlist = [player1, player2, player3]
+# player1 = Player('Pierce', [], 500)
+# player2 = Player('Margaret',[], 500)
+# player3 = Player('Cubano', [], 500)
+# playerlist = [player1, player2, player3]
 
-class Dealer:
-    def embaralhar_Cartas(self):
-        random.shuffle(deck)
-    def deal_Cards(self):
-        for n in playerlist:
-            carta1 = deck.pop(0)
-            carta2 = deck.pop(0)
-            n.hand.append(carta1)
-            n.hand.append(carta2)
+names =['Pierce','Margaret','Cubano']
+players = [Player(names[n], [], 500) for n in range(3)]
+
 dealer = Dealer()
-dealer.embaralhar_Cartas()
-dealer.deal_Cards()
-player3.see_Hand()
-player2.see_Hand()
-player1.see_Hand()
+dealer.embaralhar_Cartas(deck)
+dealer.deal_Cards(players,deck)
+
+for n in range(3):
+    players[n].see_hand()
+
 print(len(deck))
 #para o deck =
 #         |A .  | _____
